53a48bf664792a79d7a55c0a0cc193124f3f99cf
Letam Bossman Barinua <letambarinua@gmail.com>
2023-03-19 15:06:49 +0100

    Create assets and fonts directory to put images and fonts respectively

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import App from "./components/App/App";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
+import "./globalStyles/index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
