385c7fbdc90667d1b6dd6b921050ca0ede57eccf
Letam Bossman Barinua <letambarinua@gmail.com>
2023-03-19 23:41:25 +0100

    Create LendSqrLogo component to display LendSqr logo at top left page

---
+++ src/components/Login/LendSqrLogo.tsx
@@ -0,0 +1,12 @@
+import Logo from "../../assets/login-assets/Group.svg";
+
+export default function LendSqrLogo() {
+  return (
+    <div>
+      <img
+        src={Logo}
+        alt="Lend square logo"
+      />
+    </div>
+  );
+}
