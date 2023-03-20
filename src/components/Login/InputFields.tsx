f3b2b99b0ef67d8407eb6f46f5e85d37d4141e80
Letam Bossman Barinua <letambarinua@gmail.com>
2023-03-20 04:18:37 +0100

    Add ability to show/hide password when logging in

--- src/components/Login/InputFields.tsx
+++ src/components/Login/InputFields.tsx
@@ -1,13 +1,31 @@
+import { useState } from "react";
 import { loginFields } from "../../constant/textConstant";
 
 export default function InputFields() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  // Define function to handle password visibility toggle
+  const handlePasswordVisibility = () => setShowPassword(!showPassword);
+
+  // Define function to display input fields
   const displayInputFields = (loginField: string) => (
     <div key={loginField}>
       <input
-        type={loginField}
+        type={
+          loginField === "Password"
+            ? showPassword
+              ? "text"
+              : "password"
+            : loginField
+        }
         placeholder={loginField}
       />
-      {loginField === "Password" && <span>Show</span>}
+      {loginField === "Password" && (
+        // Show/hide password toggle
+        <span onClick={handlePasswordVisibility}>
+          {showPassword ? "Hide" : "Show"}
+        </span>
+      )}
     </div>
   );
 
