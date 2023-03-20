da120a67f13826e14f3b83d75be8a12565e60a5a
Letam Bossman Barinua <letambarinua@gmail.com>
2023-03-20 11:34:03 +0100

    Create UserNotification component for displaying notification
    This component will also display user avatar along side a documentation
    link

---
+++ src/components/Dashboard/UserNotification.tsx
@@ -0,0 +1,24 @@
+import UserAvatar from ".././../assets/dashboard-assets/user-avatar.png";
+import ChevronDown from ".././../assets/dashboard-assets/chevron-down.png";
+import NotificationIcon from ".././../assets/dashboard-assets/notification-icon.png";
+
+export default function UserNotification() {
+  return (
+    <div>
+      <p>Docs</p>
+      <img
+        src={NotificationIcon}
+        alt="Notification Icon"
+      />
+      <img
+        src={UserAvatar}
+        alt="Notification Icon"
+      />
+      <p>Adedeji</p>
+      <img
+        src={ChevronDown}
+        alt="Notification Icon"
+      />
+    </div>
+  );
+}
