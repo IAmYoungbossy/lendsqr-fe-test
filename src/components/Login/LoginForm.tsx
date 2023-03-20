608a9c307fde38158a56a269a2f335135d18e94b
Letam Bossman Barinua <letambarinua@gmail.com>
2023-03-20 03:42:46 +0100

    Add media query rules to elements to make them responsive to screens

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -1,12 +1,14 @@
 import "./styles/LoginForm.css";
 import InputFields from "./InputFields";
 import LoginButton from "./LoginButton";
+import LendSqrLogo from "./LendSqrLogo";
 import LoginInstruction from "./LoginInstruction";
 
 export default function LoginForm() {
   return (
     <form className="login-form">
       <div>
+        <LendSqrLogo />
         <LoginInstruction />
         <InputFields />
         <p>Forgot Password?</p>
