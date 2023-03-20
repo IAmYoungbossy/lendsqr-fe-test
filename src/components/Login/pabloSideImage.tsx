5a35521545082e57f7345ff117c9a9a67b6908a0
Letam Bossman Barinua <letambarinua@gmail.com>
2023-03-20 02:35:03 +0100

    Make Pablo sign in image responsive

--- src/components/Login/pabloSideImage.tsx
+++ src/components/Login/pabloSideImage.tsx
@@ -1,16 +1,12 @@
 import "./styles/PabloSideImage.css";
 import LendSqrLogo from "./LendSqrLogo";
-import PabloImage from "../../assets/login-assets/pablo-sign-in.svg";
 
 export default function PabloSideImage() {
   return (
     <div className="pablo-sign-in">
       <LendSqrLogo />
       <div>
-        <img
-          src={PabloImage}
-          alt="Pablo Sign In"
-        />
+        <div />
       </div>
     </div>
   );
