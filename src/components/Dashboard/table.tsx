680f25864b20ff8fa9d9f7ba506a0e28e449efa6
Letam Bossman Barinua <letambarinua@gmail.com>
2023-03-21 17:18:36 +0100

    Add styles to table in dashboard

--- src/components/Dashboard/table.tsx
+++ src/components/Dashboard/table.tsx
@@ -1,28 +1,31 @@
+import "./styles/Table.css";
 import { tableText } from "../../constant/textConstant";
 import ThreeDots from "../../assets/dashboard-assets/table-icons/3dots-details-icon.png";
 
 export default function Table() {
   // Table Body
-  const tableData = () => <ContactRow />;
+  const tableData = (data: number) => <ContactRow key={data} />;
 
   //   Table Column Header
   const tableHeader = (text: string) => (
     <th key={text}>
-      {text}{" "}
-      <img
-        src={tableText.iconUrl}
-        alt="Filter Icon"
-      />
+      <div>
+        {text}
+        <img
+          src={tableText.iconUrl}
+          alt="Filter Icon"
+        />
+      </div>
     </th>
   );
-
+  const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
   return (
-    <div>
+    <div className="table-container">
       <table>
         <thead>
           <tr>{tableText.header.map(tableHeader)}</tr>
         </thead>
-        <tbody>{tableText.header.map(tableData)}</tbody>
+        <tbody>{array.map(tableData)}</tbody>
       </table>
     </div>
   );
@@ -31,17 +34,29 @@ export default function Table() {
 export function ContactRow() {
   return (
     <tr>
-      <td>Lendsqr</td>
-      <td>Adedeji</td>
-      <td>adedeji@lendsqr.com</td>
-      <td>07067612139</td>
-      <td>May 15 2020 10:00 Am</td>
       <td>
-        <div>Active</div>
-        <img
-          src={ThreeDots}
-          alt="Details"
-        />
+        <span>Lendsqr</span>
+      </td>
+      <td>
+        <span>Adedeji</span>
+      </td>
+      <td>
+        <span>adedeji@lendsqr.com</span>
+      </td>
+      <td>
+        <span>07067612139</span>
+      </td>
+      <td>
+        <span>May 15 2020 10:00 Am</span>
+      </td>
+      <td>
+        <div>
+          <div>Active</div>
+          <img
+            src={ThreeDots}
+            alt="Details"
+          />
+        </div>
       </td>
     </tr>
   );
