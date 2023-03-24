6bdfff2f6c539023fef970b7bd2828e0d1eadc95
Letam Bossman Barinua <letambarinua@gmail.com>
2023-03-24 17:17:41 +0100

    Add ability to toggle filter form modal

--- src/components/Dashboard/FilterForm.tsx
+++ src/components/Dashboard/FilterForm.tsx
@@ -1,19 +1,28 @@
 import "./styles/FilterForm.css";
+import { BiChevronDown } from "react-icons/bi";
 import { filterInputFields } from "../../constant/textConstant";
 
 export default function FilterForm() {
   return (
-    <form className="filter-form">
+    <form
+      className="filter-form"
+      onClick={(e) => e.stopPropagation()}
+    >
       {filterInputFields.map((inputField) => (
         <div key={inputField.label}>
           <label htmlFor={inputField.label}>
             {inputField.label}
           </label>
-          <input
-            id={inputField.label}
-            type={inputField.type}
-            placeholder={inputField.placeholder}
-          />
+          <div>
+            <input
+              id={inputField.label}
+              type={inputField.type}
+              placeholder={inputField.placeholder}
+            />
+            {inputField.type === "select" && (
+              <BiChevronDown />
+            )}
+          </div>
         </div>
       ))}
       <div>
